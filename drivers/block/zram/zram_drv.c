3082c5c46a0f6cb1b668ac88e458436dd6e591cd
DawnT112 <3193782263@qq.com>
2025-09-21 12:51:12 +0800

    SET Zram to 8G SIZE

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1807,7 +1807,7 @@ static ssize_t disksize_store(struct device *dev,
 	struct zram *zram = dev_to_zram(dev);
 	int err;
 
-	disksize = (u64)SZ_1G * 3;
+	disksize = (u64)8192 * SZ_1M;
 
 	down_write(&zram->init_lock);
 	if (init_done(zram)) {
